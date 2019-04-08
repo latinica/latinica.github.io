7b6d4a74e3a579b1600df59d8dd2435c57e40d63
latinica <48415237+latinica@users.noreply.github.com>
2019-04-08 13:56:49 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ function perevod()
 	var sh_flag = 0;
 	var sch_flag = 0;
     var cyrArray = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'];
-    var latArray = ['a','b','v','g','d','e','jo','ž','z','i','j','k','l','m','n','o','p','r','s','t','u','f','h','c','č','š','š','’','y','ǐ','ě','ju','ja']
+    var latArray = ['a','b','v','g','d','e','jo','ž','z','i','j','k','l','m','n','o','p','r','s','t','u','f','h','c','č','š','š','’','y','ǐ','ě','ju','ja'];
     var cyrArray2 = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я'];
     var latArray2 = ['A','B','V','G','D','E','Jo','Ž','Z','I','J','K','L','M','N','O','P','R','S','T','U','F','H','C','Č','Š','Š','','Y','','Ě','Ju','Ja'];
-	var sh_replace = ['а'
+    var cyrConsonants = ['б', 'в', 'г', 'д', 'ж', 'з', 'й', 'к', 'л', 'м', 'н', 'п', 'р', 'с', 'т', 'ф', 'х', 'ц', 'ч', 'ш', 'щ'];
+    var latConsonants = ['b', 'v', 'g', 'd', 'ž', 'z', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'f', 'h', 'c', 'č', 'š', 'š'];
     p = document.getElementById('latinica');
     //1 step: replacing cyrillic letters with identical latin letters
     for (var i = 0; i < inputString.length; i++)
@@ -48,6 +49,12 @@ function perevod()
 				{
 					currentLetter == 'ю'
 				}
+				if (consonants.indexOf(currentLetter) > -1)
+            			{
+             			        number = consonants.indexOf(currentLetter);
+              			        resultLetter = latArray[number];
+					resultString += resultLetter;
+           			}
 			}
 		}
 		
